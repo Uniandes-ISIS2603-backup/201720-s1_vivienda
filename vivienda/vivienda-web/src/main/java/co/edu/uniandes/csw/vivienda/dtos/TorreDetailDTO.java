e4eae5383b4c7b97e3f3bd32d68e20f9d85ee010
DIEGO ALEJANDRO SOLANO ARIZA <da.solano1@sis.virtual.uniandes.edu.co>
2017-09-18 08:43:43 -0500

    DTOs de torre

---
+++ vivienda/vivienda-web/src/main/java/co/edu/uniandes/csw/vivienda/dtos/TorreDetailDTO.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.vivienda.dtos;
+
+import co.edu.uniandes.csw.vivienda.entities.TorreEntity;
+
+/**
+ *
+ * @author da.solano1
+ */
+public class TorreDetailDTO extends TorreDTO {
+    public TorreDetailDTO(){
+        
+    }
+    public TorreDetailDTO(TorreEntity torre){
+        super(torre); 
+    }
+    public TorreEntity toEntity(){
+        TorreEntity TorreE = super.toEntity(); 
+        return TorreE; 
+    }
+}
---
+++ vivienda/vivienda-web/src/main/java/co/edu/uniandes/csw/vivienda/resources/RestConfig.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.vivienda.resources;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+
+/**
+ *
+ * @author da.solano1
+ */
+@ApplicationPath("/api")
+public class RestConfig extends Application{
+}
