7ceacec348be173c70c03c2e404c6d48ef7aef39
rj.gonzalez10 <rj.gonzalez10@ISIS2603S1-0035.sis.virtual.uniandes.edu.co>
2017-11-29 00:06:42 -0500

    Se empieza a implementar la busqueda por nombre de servicio

--- vivienda-web/src/main/webapp/src/modules/Servicio/servicio.ctrl.js
+++ vivienda-web/src/main/webapp/src/modules/Servicio/servicio.ctrl.js
@@ -9,11 +9,38 @@
             });
             
             if (($state.params.servicioId !== undefined)&& ($state.params.servicioId !== null)) {
-                $http.get("http://localhost:8080/vivienda-web/api/mensajes" + '/' + $state.params.servicioId).then(function (response) {
-                    $scope.currentservicio = response.data;
+                $http.get("http://localhost:8080/vivienda-web/api/servicios" + '/' + $state.params.servicioId).then(function (response) {
+                    $scope.currentServicio = response.data;
                     $scope.servicioadmin = $scope.currentServicio.admin;
                 });
             }
+            
+            $scope.buscarServicio = function () {
+                $http.get("http://localhost:8080/vivienda-web/api/servicios").then(function (response) {
+                    $scope.todosServicios = response.data;
+                    var bool = false;
+                    for(var pos = 0;pos < $scope.todosServicios.length; pos++)
+                    {
+                        if($scope.todosServicios[pos].titulo === $scope.mensajeBuscarTitulo)
+                        {
+                            $scope.currentServicio = $scope.todosServicios[pos];
+                            bool = true;
+                            break;
+                        }
+                    }
+                    if(bool === true)
+                    {
+                        $state.go('servicioDetail', {servicioId: $scope.currentServicio.id}, {reload: true});
+                    }
+                    else
+                    {
+                        $state.go('servicioError', {adminId: null}, {reload: true});
+                    }
+                }, function()
+                {
+                    $state.go('servicioError', {adminId: null}, {reload: true});
+                });
+            };
         }
     ]);
 }
--- vivienda-web/src/main/webapp/src/modules/Servicio/servicio.new.ctrl.js
+++ vivienda-web/src/main/webapp/src/modules/Servicio/servicio.new.ctrl.js
@@ -4,35 +4,30 @@
     mod.controller('servicioNewCtrl', ['$scope', '$http', 'servicioContext', '$state', '$rootScope',
         function ($scope, $http, servicioContext, $state, $rootScope) {
             $rootScope.edit = false;
-            $http.get("http://localhost:8080/vivienda-web/api/prestadores").then(function (response) {
+            
+           $http.get("http://localhost:8080/vivienda-web/api/prestadores").then(function (response) {
                 
                 $scope.prestadoresRecords = response.data;
             });
-            $http.get("http://localhost:8080/vivienda-web/api/prestadores").then(function (response) {
-                    $scope.servicioadmin2 = response.data;});
+            
             $scope.createServicio = function () {
-                try
-                {
-                $http.get("http://localhost:8080/vivienda-web/api/prestadores" + '/' + $scope.servicioAdmin).then(function (response) {
-                    $scope.servicioadmin = response.data;
-                });
                 
-                if($scope.servicioadmin !== undefined && $scope.servicioadmin !== null)
-                {
-                $http.post("http://localhost:8080/vivienda-web/api/servicios", {
+               $http.get("http://localhost:8080/vivienda-web/api/prestadores" + '/' + $scope.servicioAdmin).then(function (response) {
+                    $scope.servicioadmin = response.data;
+                
+                
+                     $http.post("http://localhost:8080/vivienda-web/api/servicios", {
                     nombre: $scope.servicioId,
                     precio: $scope.servicioPrecio,
                     admin: $scope.servicioadmin
                 }).then(function (response) {
                     $state.go('servicioList', {servicioId: response.data.id}, {reload: true});
+                    }, function()
+                    {
+                        console.log("Fallo");
+                        $state.go('servicioError', {servicioId: false}, {reload: true});
+                    });
                 });
-                }
-                
-                }
-                catch(Error)
-                {
-                    $state.go('servicioList', {servicioId: null}, {reload: true});
-                }
             };
         }
     ]);
